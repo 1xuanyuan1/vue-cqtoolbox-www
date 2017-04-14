7298af186584ee9b2257630f3196f8f44eab751d
Duke <546407233@qq.com>
2017-04-14 17:27:08 +0800

    ✨  [feature] init

---
+++ src/components/Hello.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <editor v-model="code"></editor>
+    <preview v-model="code" debounce="500"></preview>
+  </div>
+</template>
+
+<script>
+import Editor from '@/components/editor'
+import Preview from '@/components/preview'
+export default {
+  name: 'hello',
+  components: {
+    Editor,
+    Preview
+  },
+  data () {
+    return {
+      code: `
+<md-button class="md-raised md-primary">Hello</md-button>
+<md-button class="md-raised md-warn">Hello</md-button>
+`
+    }
+  }
+}
+</script>
